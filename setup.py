14a34c106c483c547e857f8cd830262fe03aa365
detailyang <detailyang@gmail.com>
2016-09-23 20:27:44 +0800

    release 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pix-css',
-    version='0.1.0',
+    version='0.1.1',
     keywords=('pixel css art'),
     description='transform picture to pixel cs',
     license='MIT License',
